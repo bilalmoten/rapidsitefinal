2392ed62bbd6f57f023f5474b3375e4050db3d36
bilalmoten <82532206+bilalmoten@users.noreply.github.com>
2024-11-08 17:25:22 +0500

    aadding sitemap generation..

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://aiwebsitebuilder.tech', // Replace with your website's URL
+    generateRobotsTxt: true,           // Generates a `robots.txt` file
+    sitemapSize: 5000                 // Max number of entries per sitemap
+};
