c24be8e2b3ef37680e27bc8dff3d430ff4788d6e
bilalmoten <82532206+bilalmoten@users.noreply.github.com>
2024-11-08 21:33:36 +0500

    fixing sitemap further

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,33 @@
-// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://aiwebsitebuilder.tech', // Replace with your website's URL
-    generateRobotsTxt: true,           // Generates a `robots.txt` file
-    sitemapSize: 5000                 // Max number of entries per sitemap
-};
+    siteUrl: 'https://aiwebsitebuilder.tech',
+    generateRobotsTxt: true,
+    exclude: [
+        '/404',
+        '/500',
+        '/api/*',
+        '/dashboard/*',
+        '/protected/*',
+        '/login/*',
+        '/signup/*',
+    ],
+    robotsTxtOptions: {
+        additionalSitemaps: [],
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: [
+                    '/api/*',
+                    '/dashboard/*',
+                    '/protected/*',
+                    '/login/*',
+                    '/signup/*',
+                ],
+            },
+        ],
+    },
+    changefreq: 'daily',
+    priority: 0.7,
+    generateIndexSitemap: false, // Since your site is small, we don't need an index
+}
