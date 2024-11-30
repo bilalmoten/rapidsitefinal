86cf5dd26cf81635f6845777bf3cd4748e150bd4
bilalmoten <82532206+bilalmoten@users.noreply.github.com>
2024-11-30 20:27:26 +0500

    landing page updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,45 @@
-import { createClient } from "@/utils/supabase/server";
-import LandingPage from "@/components/LandingPage";
+// import { createClient } from "@/utils/supabase/server";
+// import LandingPage from "@/components/LandingPage";
 
-export default async function Index() {
-  const supabase = await createClient();
+// export default async function Index() {
+//   const supabase = await createClient();
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+//   const {
+//     data: { user },
+//   } = await supabase.auth.getUser();
 
-  return <LandingPage user={user} />;
+//   return <LandingPage user={user} />;
+// }
+
+"use client";
+
+import Layout from "@/components/landing/Layout";
+import Hero from "@/components/landing/Hero";
+import ValueProposition from "@/components/landing/ValueProposition";
+import HowItWorks from "@/components/landing/HowItWorks";
+import Features from "@/components/landing/Features";
+import Showcase from "@/components/landing/Showcase";
+import Pricing from "@/components/landing/Pricing";
+import FAQ from "@/components/landing/FAQ";
+import CTA from "@/components/landing/CTA";
+// import ThemePreview from "@/components/landing/ThemePreview";
+import EditorShowcase from "@/components/landing/EditorShowcase/index";
+import Testimonials from "@/components/landing/Testimonials";
+
+export default function LandingPage() {
+  return (
+    <Layout>
+      <Hero />
+      {/* <ThemePreview /> */}
+      <HowItWorks />
+      <EditorShowcase />
+      <Showcase />
+      <Testimonials />
+      <FAQ />
+      <Features />
+      <Pricing />
+      <ValueProposition />
+      <CTA />
+    </Layout>
+  );
 }
